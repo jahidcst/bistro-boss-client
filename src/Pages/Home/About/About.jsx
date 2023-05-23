f0880ebd6205ccf7da44bc9ede5a318d3ce7d023
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-23 19:58:45 +0600

    chef added

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,20 @@
+import chef from '../../../assets/home/chef-service.jpg'
+const About = () => {
+    return (
+        <div>
+            <div className="hero max-h-screen">
+                <img className='h-[370px] w-[1320px]' src={chef} alt="" />
+
+                <div className="hero-content text-center text-neutral-content">
+                    <div className=" bg-white text-black py-8 bg-opacity-40 ">
+                        <h1 className='text-4xl font-semibold mb-3 uppercase'>Bistro Boss</h1>
+                        <p className="mb-5  px-24 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Category></Category>
+            <About></About>
         </div>
     );
 };
