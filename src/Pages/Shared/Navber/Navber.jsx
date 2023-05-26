29278760bb32cfa31fc66d1ca80d3751b3650c3d
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-26 17:25:09 +0600

    perfection

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import img from '../../../assets/icon/logo.png'
 
 
 const Navber = () => {
@@ -27,9 +28,7 @@ const Navber = () => {
             <div className="navbar max-w-screen-lg fixed z-10  text-white bg-opacity-25 bg-black">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
+                        <Link><img  className="w-12 ml-5" src={img} alt="" /></Link>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {navOptions}
                         </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,35 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#DB924B",
+
+          "secondary": "#263E3F",
+
+          "accent": "#10576D",
+
+          "neutral": "#120C12",
+
+          "base-100": "#20161F",
+
+          "info": "#8DCAC1",
+
+          "success": "#9DB787",
+
+          "warning": "#FFD25F",
+
+          "error": "#FC9581",
+        },
+      },
+    ],
   },
+
+
   plugins: [require("daisyui")],
 }
+
 
